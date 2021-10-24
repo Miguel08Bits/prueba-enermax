5ffe9645f286e15e67c1abff35c57f7cf2af1c54
Miguel08Bits <ma.navarropacheco@gmail.com>
2021-10-24 14:13:44 -0500

    Instalación de PrimeNG V7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
